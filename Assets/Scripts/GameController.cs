e8d61d028136f49567385049604ce3dad78ec4fa
Xenoxido <enric.bnt@gmail.com>
2020-01-16 17:36:58 +0100

    JejePene

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject capRed;
+    public GameObject capBlue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        capRed = (GameObject)Instantiate(capRed);
+        capRed.AddComponent<MeshRenderer>();
+        capRed.transform.parent = this.transform;
+        capRed.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
