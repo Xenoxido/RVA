81c2d033973865fc43697c92bdb1fc97a7e7c8a9
Jose Gómez <josegomezgadea@gmail.com>
2020-01-22 21:32:56 +0100

    Mierda, no va

---
+++ Assets/Scripts/CollisionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Text marcador;
+
+    void Start()
+    {
+
+        marcador = GameObject.FindGameObjectWithTag("Marcador").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        marcador.text = (Int32.Parse(marcador.text)+1).ToString();
+        StartCoroutine(Die());
+    }
+
+    private IEnumerator Die()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,25 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Vuforia;
 
 public class GameController : MonoBehaviour
 {
     public GameObject capRed;
-    public GameObject capBlue;
+    public GameObject bala;
+
+    private float dist;
+    private Vector3 v3Offset;
+    private Plane plane;
+    public float thrust = 1.0f;
+    public Button fireButton;
+    public int lastTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        capRed = (GameObject)Instantiate(capRed);
-        capRed.AddComponent<MeshRenderer>();
-        capRed.transform.parent = this.transform;
-        capRed.SetActive(true);
-
+        lastTime = System.DateTime.Now.Second;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (System.DateTime.Now.Second - lastTime > 1)
+        {
+            float x = UnityEngine.Random.Range(-0.5f, 0.5f);
+            float z = UnityEngine.Random.Range(-0.28f, 0.28f);
+            GenerarCapRed(x, z);
+            lastTime = System.DateTime.Now.Second;
+        }
     }
+
+    public void GenerarCapRed(float x, float z)
+    {
+        GameObject cap = (GameObject)Instantiate(capRed);
+        //cap.transform.position = this.transform.position + new Vector3(x, 0, z);
+        cap.transform.SetPositionAndRotation(this.transform.position /*+ new Vector3(x, 0, z)*/, this.transform.rotation);
+        //cap.transform.position = new Vector3(x, 0, z);
+        cap.transform.parent = this.transform;
+        cap.transform.position += new Vector3(x, 0, z);
+        cap.SetActive(true);
+    }
+
+
+    public void OnFireButton()
+    {
+        fireButton.interactable = false;
+        GameObject bullet = (GameObject)Instantiate(bala);
+        bullet.transform.position = Camera.main.transform.position;
+        bullet.transform.parent = this.transform;
+        bullet.SetActive(true);
+        plane.SetNormalAndPosition(Camera.main.transform.forward, bullet.transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        plane.Raycast(ray, out dist);
+        v3Offset = transform.position - ray.GetPoint(dist);
+        bullet.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * thrust, ForceMode.Impulse);
+        StartCoroutine(Cooldown());
+    }
+
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(1);
+        fireButton.interactable = true;
+    }
+
 }
